1884815517664a7bcab37931aac9830e9df8661d
Ton <gotton@foxmail.com>
2023-11-15 21:13:50 +0800

    attr

--- Vue/vueDeepStudy/src/views/DemoView.vue
+++ Vue/vueDeepStudy/src/views/DemoView.vue
@@ -5,19 +5,36 @@
         <div :value="bindNull">bind null</div>
         <div :value="bindObj">bind object</div>
         <input v-bind="inputObj">
-        <input v-bind="inputObj" type="number">
+        <br/><br/>
+        <button @click="changeStatus">change status</button>
+        <div :class="{ 'active-status': divActive}" class="status-block">active</div>
     </div>
 </template>
   
 <script setup>
+import {ref, computed } from 'vue'
 let bindValue = 'bind String'
 let bindNull = ''
 let bindObj = {a:'a', b:'b'}
-let inputObj = {name:"user-name", type: 'text', placeholder:'please input your name'}
+let inputObj = {
+    name:"user-name", 
+    type: 'text', 
+    placeholder:'please input your name'
+    }
+let divActive = ref(false)
+function changeStatus(){
+    console.log('changeStatus is running')
+    divActive.value = !divActive.value
+}
 </script>
   
 <style>
 .home-page {
     flex-direction: column;
+}
+.status-block{
+    width: 120px;
+    height: 30px;
+    margin-top: 15px;
 }
 </style>
