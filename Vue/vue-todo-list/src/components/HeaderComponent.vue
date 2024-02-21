d1a33e4875fc90c87f534eaf1aee5254451852e9
Ton <gotton@foxmail.com>
2024-02-21 17:35:23 +0800

    vue

--- Vue/vue-todo-list/src/components/HeaderComponent.vue
+++ Vue/vue-todo-list/src/components/HeaderComponent.vue
@@ -1,22 +1,29 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 const emit = defineEmits(['addItem'])
   const newTodo = ref('')
 const handleClick = (param,param1) => {(
     console.log(`${param}th box is clicked`,param1)
 )}
+const isGolden = ref(false)
+const demoObj = reactive({name:'ton',age:23})
+function addProp(){
+  Object.assign(demoObj,{company:'dance'})
+}
 </script>
 
 <template>
   <h1 class="logo">To Do List</h1>
   <div class="container">
-    <div v-for="item in [1,2,3]" class="box" @click="handleClick(item,'yes')"></div>
+    <div v-for="item of [10,20,30]" class="box" :class="`box${item}`"  @click="handleClick(item,'yes')"></div>
   </div>
   <hr/>
   <input 
     v-model=newTodo
     type="text" placeholder="请输入新的To do项，回车新增" 
     class="todo-input" @keydown.enter="($emit('addItem',newTodo), newTodo='')">
+  <button @click="addProp">greet</button>
+  <div v-for="item in demoObj">{{item}}</div>
 </template>
 
 
@@ -27,11 +34,9 @@ const handleClick = (param,param1) => {(
   border: 1px solid #42b883aa;
 }
 .box:first-child{
-  opacity: 0;
   background: #33228899;
 }
 .box:nth-child(2){
-  visibility: hidden;
   background: #aa99bb;
 }
 .box:nth-child(3){
