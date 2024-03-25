f9ac48028091b3baf929a0e82ece9d38598589d7
Ton <gotton@foxmail.com>
2024-03-25 17:50:02 +0800

    初始化全局scss文件

--- qiankun/base-app/src/App.vue
+++ qiankun/base-app/src/App.vue
@@ -6,12 +6,36 @@ const testFunction = () => {
 </script>
 
 <template>
-    <h1>app.vue</h1>
+    <div class="main-app">
+        <nav>nav</nav>
+        <aside>aside</aside>
+        <main>main
+            <div id="sub-app"></div>
+        </main>
+
+    </div>
 </template>
 
-<style lang="scss">
-h1 {
-    color: #33aabb;
-    font-size: 30px;
+<style lang="scss" scoped>
+.main-app{
+    height: 100vh;
+    display: grid;
+    grid-template-areas: 
+        'nav nav'
+        'aside main';
+    grid-template-columns: 12% 1fr;
+    grid-template-rows: 8% 1fr;
+    nav{
+        grid-area: nav;
+        background-color: #fce38a;
+    }
+    aside {
+        grid-area: aside;
+        background-color: #eaffd0;
+    }
+    main {
+        grid-area: main;
+        background-color: #95e1d3;
+    }
 }
 </style>
