ef09cd08dddfc29e36a2d62427b3869cb80f6be0
ton <gotton@foxmail.com>
2024-03-27 00:04:54 +0800

    emit props

--- TypeScript/heima-ts-programe/src/App.vue
+++ TypeScript/heima-ts-programe/src/App.vue
@@ -2,14 +2,17 @@
   import NavBar from './components/NavBar.vue';
   import ContentList from './components/ContentList.vue'
   import useStore from './store'
+  import  {ref} from 'vue'
 
   const { channels } = useStore()
   console.log('channels', channels.channels)
+  const demoNumber = ref<number>()
 </script>
 
 <template>
   <NavBar></NavBar>
-  <ContentList></ContentList>
+  <p>{{demoNumber}}</p>
+  <ContentList v-model="demoNumber"></ContentList>
 </template>
 
 <style scoped>
--- TypeScript/heima-ts-programe/src/components/ContentList.vue
+++ TypeScript/heima-ts-programe/src/components/ContentList.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 import ArticleItem from './ArticleItem.vue';
 const items = [{id:3392},{id:31392},{id:33392},{id:334592}]
+const addValue = () => {}
+interface Props {
+    modelValue:number
+}
+defineProps<Props>()
 </script>
 
 <template>
     <main>
+        <div class="demo">
+            <button @click="addValue">add</button>
+            <input type="text">
+            <button @click="addValue">minus</button>
+        </div>
         <article-item v-for="item in items" :key="item.id" :item="item" />
     </main>
 </template>
